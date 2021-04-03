00f722fa2fbce3e5826bbb0b44af26630eeea5a6
OrtwIn <bertil.braun.private@gmail.com>
2021-04-04 00:07:47 +0200

    Speach Wrapper

--- src/speech.py
+++ src/speech.py
@@ -1,11 +1,9 @@
 from gtts import gTTS
 from playsound import playsound
   
-mytext = 'Welcome!'
-language = 'en'
 
-# generate speech from text and save to speech.mp3
-gTTS(text=mytext, lang=language, slow=False).save('speech.mp3')
-
-# play the text
-playsound('speech.mp3')
+def play(text, lng='en'):
+    # generate speech from text and save to speech.mp3
+    gTTS(text=text, lang=lng, slow=False).save('speech.mp3')
+    # play the text
+    playsound('speech.mp3')
